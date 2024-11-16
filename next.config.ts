7d4065234c0df5ab9543680bdfc89662a32a0a77
Roman Urban-Orban <roman.urban.orban@gmail.com>
2024-11-16 02:47:40 +0200

    fix build

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,8 @@
-import path from 'path';
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Any other Next.js config options can go here
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...(config.resolve.alias || {}),
-      '@': path.resolve(__dirname),
-    };
-    return config;
-  },
+  /* config options here */
 };
 
 export default nextConfig;
+
