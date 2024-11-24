7f5090b58fb9fdebfd72a4566caebccbe96746a9
Roman Urban-Orban <roman.urban.orban@gmail.com>
2024-11-24 20:46:02 +0200

    load dashboard by default

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
+import Dashboard from './dashboard/page';
+
 export default function Home() {
-  return (
-    <div className="flex-1 p-4">
-      <h1 className="text-2xl font-bold">Welcome to the Home Page</h1>
-      {/* Home page content */}
-    </div>
-  );
+  return <Dashboard />;
 }
